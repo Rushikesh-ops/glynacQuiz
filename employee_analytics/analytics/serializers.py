ce7de4e6b5abbc0e50f907bc47730a692c0ad325
Rushikesh Bhoskar <rpbhoskar@gmail.com>
2025-04-19 21:13:21 +0530

    Added serializers

--- employee_analytics/analytics/serializers.py
+++ employee_analytics/analytics/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
-from .models import Employee
+from .models import *
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = '__all__' 
+
+
+class PerformanceReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerformanceReview
+        fields = '__all__'  
+
+
+class ProjectAssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectAssignment
+        fields = '__all__'  
+
+
+class SalaryHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalaryHistory
+        fields = '__all__'  #
